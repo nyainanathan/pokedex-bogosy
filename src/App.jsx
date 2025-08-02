3f808a30102469a32e7a51eb9ad6d7fa505df616
Daniel <Lucdanielrh01@gmail.com>
2025-08-02 15:40:11 +0200

    feat : Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/')
+        const data = await response.json()
+        setPokemon(data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching Pokemon:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchPokemon()
+  }, [])
+
+  if (loading) return <div>Loading...</div>
+  if (!pokemon) return <div>Undifinded</div>
+
+  const nbr_id = `N° ${pokemon.id.toString()}`
 
   return (
-    <>
-    </>
+    <div className="pokemon-card">
+      <img 
+  className="pokemon-image"
+/>
+      <div className="info">{nbr_id}</div>
+      <div className="info-name">{pokemon.name}</div>
+      
+      <div className="pokemon-types">
+        {pokemon.types.map((typeInfo) => (
+          <span>
+            {typeInfo.type.name}
+          </span>
+        ))}
+      </div>
+    </div>
   )
 }
 
