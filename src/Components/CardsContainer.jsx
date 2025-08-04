3b4cee90a1f6acf7d0a61d0d9daefcf1a7e17f05
nyainanathan <nyaina.nathan.1@gmail.com>
2025-08-04 12:36:59 +0300

    feat: pokemon search logic added

--- src/Components/CardsContainer.jsx
+++ src/Components/CardsContainer.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
 import SearchBar from "./SearchBar"
 
 const CardsContainer = () => {
+
+    const [pokemonList, setPokemonList] = useState([]);
+    const [searchParams, setSearchParams] = useState("");
+    const [researchedPokemon , setResearchedPokemon] = useState([]);
+
+    useEffect(() => {
+        const fetchAllPokemons = async () => {
+            try{
+                const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1302");
+                const parsedData = await data.json();
+                //console.log(parsedData.results); just for debugging purpose ^^
+                setPokemonList(parsedData.results)
+            } catch (err) {
+                console.error(err);                
+            }
+        }
+        fetchAllPokemons()
+    } , [])
+
+    useEffect(() => {
+        const newPokemons = pokemonList.filter((pokemon) => pokemon.name.toLowerCase().includes(searchParams.toLowerCase()));
+        setResearchedPokemon(newPokemons)
+    } , [searchParams, pokemonList])
+
     return (
         <div className="bg-amber-200 h-screen w-screen flex flex-col items-center p-11" >
-            <SearchBar/>
+            <SearchBar onSearch={setSearchParams}/>
+            {researchedPokemon.map((pokemon, index) => (
+                <li key={index}>{pokemon.name} : {pokemon.url}</li>
+))}
         </div>
     )
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,7 +2,7 @@ const SearchBar = ({onSearch}) => {
     return(
         <input type="text" 
                 placeholder="Enter a pokemon name ..." 
-                onChange={ (e) => onSearch(e.target.value)}
+                onChange={(e) => onSearch(e.target.value)}
                 className="w-1/3 border-2 rounded-2xl p-2 text-center"
         />
     )
