66a2166387e5a073aaca4cf250d935d7becf8bfa
Razakasoa Nekena <nyavonekena6@gmail.com>
2025-08-06 22:42:28 +0300

    SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,9 +3,10 @@ const SearchBar = ({onSearch}) => {
         <input type="text" 
                 placeholder="Enter a pokemon name ..." 
                 onChange={(e) => onSearch(e.target.value)}
-                style={{backgoundColor:'#1b263b',width:'25%' , padding:'2ch',border:'2px solid gold',borderRadius:'20px' , color:'white' }}
+                style={{backgroundColor:'#000814',width:'25%' , padding:'2ch',border:'2px solid gold',borderRadius:'20px' , color:'white' }}
         />
     )
 }
 
 export default SearchBar;
+
